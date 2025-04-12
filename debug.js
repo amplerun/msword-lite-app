9405f0743dea433f64915779f4e93ecdee4b7d45
amplerun <145580424+amplerun@users.noreply.github.com>
2025-04-12 09:37:38 +0530

    Update and rename server.js to debug.js

---
+++ debug.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+console.log('Current directory:', __dirname);
+console.log('Directory contents:');
+fs.readdirSync(__dirname).forEach(file => {
+ console.log(' - ' + file);
+});
+// Try to find server directory
+const serverPath = path.join(__dirname, 'server');
+console.log('Server directory exists:', fs.existsSync(serverPath));
+if (fs.existsSync(serverPath)) {
+ console.log('Server directory contents:');
+ fs.readdirSync(serverPath).forEach(file => {
+   console.log(' - ' + file);
+ });
+}
